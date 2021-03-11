c4f7a04e27fae95e003ff50ed10dcabd57afa9b7
harwest codeforces --setup <>
2021-03-11 16:16:00 +0600

    Add solution for problem `A - Boy or Girl` Link: https://codeforces.com/contest/236/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/236/submission/109694193

--- codeforces/236/A.cpp
+++ codeforces/236/A.cpp
@@ -1,11 +1,28 @@
 #include <bits/stdc++.h>
+
 using namespace std;
- 
+
 int main()
 {
     char c;
     set <char> username;
- 
-    while(cin>>c) username.insert(c);
+
+    while(cin>>c)
+        {username.insert(c);}
     (username.size() % 2 == 0 )? cout << "CHAT WITH HER!" <<endl : cout << "IGNORE HIM!" << endl;
+
+    return 0;
 }
+
+
+
+/***
+#include<bits/stdc++.h>
+using namespace std;
+char c;
+set<char>s;
+main(){
+while(cin>>c) s.insert(c);
+if(s.size()%2==0) cout<<"CHAT WITH HER!"; else cout<<"IGNORE HIM!";
+}
+**/
