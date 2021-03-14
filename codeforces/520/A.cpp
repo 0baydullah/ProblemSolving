db8cda01a6c8e5bf59cd04d902911b125e904b75
harwest codeforces --setup <>
2021-03-14 23:26:00 +0600

    Add solution for problem `A - Pangram` Link: https://codeforces.com/contest/520/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/520/submission/109989277

--- codeforces/520/A.cpp
+++ codeforces/520/A.cpp
@@ -1,13 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int main()
 {
     char l;
     int x;
- 
-    set <char> s;cin >>x;
+
+    set <char> s;
+    cin >>x;
     while(cin>>l)
-        {l=tolower(l);s.insert(l);}
+        {l=tolower(l);
+        s.insert(l);}
   s.size()==26 ? cout << "YES" : cout << "NO";
 }
