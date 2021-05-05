d9dca1d0fa3a972dbed5c2d364c439720d33d83e
harwest codeforces --setup <>
2021-05-05 21:33:00 +0600

    Add solution for problem `A - Do Not Be Distracted!` Link: https://codeforces.com/contest/1520/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1520/submission/115279192

--- codeforces/1520/A.cpp
+++ codeforces/1520/A.cpp
@@ -1,23 +1,24 @@
- 
- 
+
+
+
 #include<bits/stdc++.h>
 #define endl "\n"
 using namespace std;
 int main ()
 {
     ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
- 
+
     int t;
     cin >>t;
     lbl:
     while (t--)
     {
- 
+
         int n;
         cin >> n;
         string a,x;
         cin >> a;
- 
+
         for (int i= 0 ; i<n ; i++)
         {
                 for (int j= i+1 ; j<n ; j++)
@@ -27,12 +28,12 @@ int main ()
                     cout << "NO" <<endl;
                    goto lbl;
                 }
- 
+
             }
- 
+
         }
         cout << "YES" <<endl;
     }
- 
+
     return 0;
 }
