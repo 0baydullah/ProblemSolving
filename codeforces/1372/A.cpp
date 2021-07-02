f27743438a697896373ea2ff3bfdcfd4b4f259ff
harwest codeforces --setup <>
2021-07-02 22:38:00 +0600

    Add solution for problem `A - Omkar and Completion` Link: https://codeforces.com/contest/1372/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1372/submission/121129757

---
+++ codeforces/1372/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define fuckl "\n"
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+
+    int t; cin >> t;
+    while(t--)
+    {
+        int n; cin >> n;
+        while(n--)cout << 1 << " ";
+        cout << fuckl;
+    }
+
+    return 0;
+}
