0762077ff0fba017ae0adcf5069ec4c3b9b535c1
0baydullah <obaydullahkhaan@gmail.com>
2022-10-20 20:30:00 +0600

    x

---
+++ C0DEF0RCES/2022-10 (october)/1743 a.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+
+#define endl "\n"
+#define pb push_back
+
+#define PI acos(-1)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+
+#define ll long long
+#define ull unsigned long long
+
+using namespace std;
+
+void miryoku()
+{
+    int n,x; cin >> n; 
+    for(int i=0 ; i<n; i++) cin >> x;
+    n=10-n;
+    cout << n*(n-1)*3 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);  cin.tie(NULL);  cout.tie(NULL);
+
+    int T=1;
+    cin >> T;
+    for(int t=1 ; t <= T ; t++){
+    //  cout << "Case " << t << ": ";
+        miryoku();
+    }
+
+    return 0;
+}
